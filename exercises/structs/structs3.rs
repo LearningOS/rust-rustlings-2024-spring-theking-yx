c3b0364fa3654700d6a9a77ff0f86730e5f0997f
Your Name <you@example.com>
2024-04-21 01:17:13 +0800

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -29,11 +28,13 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
         // Something goes here...
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        return self.weight_in_grams * cents_per_gram
         // Something goes here...
     }
 }
